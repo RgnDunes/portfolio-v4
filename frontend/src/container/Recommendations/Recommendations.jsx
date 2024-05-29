a80a592739b315f3743a0506697e09db94224a8a
RgnDunes <divyansh.singh@razorpay.com>
2024-05-29 05:33:45 +0530

    [chore]: add testimonials code

--- frontend/src/container/Recommendations/Recommendations.jsx
+++ frontend/src/container/Recommendations/Recommendations.jsx
@@ -1,8 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { AppWrap } from "../../wrapper";
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
+
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+// import { Tooltip as ReactTooltip } from "react-tooltip";
+
 import "./Recommendations.scss";
 
-const Recommendations = () => {};
+const Recommendations = () => {
+  const [recommendations, setRecommendations] = useState([]);
+  const [currentRecommendationIdx, setCurrentRecommendationIdx] = useState(0);
 
-export default AppWrap(Recommendations, "recommendations");
+  useEffect(() => {
+    const query = '*[_type == "recommendations"]';
+
+    client.fetch(query).then((data) => {
+      setRecommendations(data);
+    });
+  }, []);
+
+  const currentTestimonial = recommendations[currentRecommendationIdx];
+
+  const handleClick = (idx) => {
+    setCurrentRecommendationIdx(idx);
+  };
+
+  return (
+    <>
+      {recommendations.length && (
+        <>
+          <div
+            className="app__testimonial-item app__flex"
+            style={{ flexDirection: "column" }}
+          >
+            <img
+              src={urlFor(currentTestimonial.imageUrl)}
+              style={{ marginBottom: "20px" }}
+            />
+
+            <div className="app__testimonial-content">
+              <p className="p-text">{`${currentTestimonial.message.substr(
+                0,
+                400
+              )}${currentTestimonial.message.length > 400 ? "..." : ""}`}</p>
+              <div>
+                <h4 className="bold-text">{currentTestimonial.name}</h4>
+                <h5 className="p-text">{currentTestimonial.designation}</h5>
+              </div>
+            </div>
+          </div>
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentRecommendationIdx === 0
+                    ? recommendations.length - 1
+                    : currentRecommendationIdx - 1
+                )
+              }
+            >
+              <HiChevronLeft />
+            </div>
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(
+                  currentRecommendationIdx === recommendations.length - 1
+                    ? 0
+                    : currentRecommendationIdx + 1
+                )
+              }
+            >
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AppWrap(
+  MotionWrap(Recommendations, "app__testimonial"),
+  "recommendations",
+  "app__primarybg"
+);
